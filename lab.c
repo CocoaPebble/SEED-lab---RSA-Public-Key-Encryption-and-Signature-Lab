63af6bd91c73492669d91c6f0e8925ff15f00042
Yingzhou Jiang <34469646+CocoaPebble@users.noreply.github.com>
2023-12-17 22:26:15 -0500

    Add printBN function to lab.c

--- lab.c
+++ lab.c
@@ -1,5 +1,14 @@
 #include "RSA.c"
 
+void printBN(char *msg, BIGNUM *a)
+{
+    /* Use BN_bn2hex(a) for hex string
+     * Use BN_bn2dec(a) for decimal string */
+    char *number_str = BN_bn2hex(a);
+    printf("%s %s\n", msg, number_str);
+    OPENSSL_free(number_str);
+}
+
 int main()
 {
     /*
